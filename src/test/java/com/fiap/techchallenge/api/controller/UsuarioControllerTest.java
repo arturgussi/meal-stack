321477af82442ea1835e93463ed8903a6703ef30
Artur Gussi <artur.oliveira@preddata.com>
2026-03-17 09:13:58 -0300

    test<fix>: Correção dos testes após upgrade do spring boot

--- src/test/java/com/fiap/techchallenge/api/controller/UsuarioControllerTest.java
+++ src/test/java/com/fiap/techchallenge/api/controller/UsuarioControllerTest.java
@@ -14,8 +14,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -35,10 +35,9 @@ class UsuarioControllerTest {
         @Autowired
         private MockMvc mockMvc;
 
-        @Autowired
-        private ObjectMapper objectMapper;
+        private ObjectMapper objectMapper = new ObjectMapper();
 
-        @MockBean
+        @MockitoBean
         private UsuarioService usuarioService;
 
         private UsuarioRequestDTO usuarioRequestDTO;
